0e494df49efceee8f3a6bb9e2074670803aa877b
Cassie Peterson <cas.j.peters@gmail.com>
2016-10-29 12:01:50 -0600

    load image faster bc of placement

--- script.js
+++ script.js
@@ -20,6 +20,13 @@ $(document).ready(function() {
         // console.log(body); //this shows that the url has changed
         //change the body object into something JSON can read
         body = JSON.stringify(body);
+        //create the image tag
+        var addImg = $("<img>");
+        //add the src to the img tag, the src being the input value
+        $(addImg).attr("src", inputVal);
+        //add the image tag into the emotion-img div
+        $('#emotion-img').append(addImg);
+        // console.log($('#emotion-img')); //make sure the img tag got into the div
         //XMLHttp Request
         var $xhr = $.post('https://g-mscog.herokuapp.com/emotion/v1.0/recognize?',
             body);
@@ -31,13 +38,6 @@ $(document).ready(function() {
             console.log(data); //log the data into the console
             //parse the body back into an object
             body = JSON.parse(body);
-            //create the image tag
-            var addImg = $("<img>");
-            //add the src to the img tag, the src being the input value
-            $(addImg).attr("src", inputVal);
-            //add the image tag into the emotion-img div
-            $('#emotion-img').append(addImg);
-            // console.log($('#emotion-img')); //make sure the img tag got into the div
         });
     };
 
