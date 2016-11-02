508da5ffe4e10b58c746ae096b9071bd7298577e
Cassie Peterson <cas.j.peters@gmail.com>
2016-11-01 22:46:46 -0600

    commit before i undo everything to try to get it to work again

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 'use strict';
+
+var createBars = false;
+
 $(document).ready(function() {
 
     //GLOBAL VARIABLES
@@ -6,31 +9,29 @@ $(document).ready(function() {
     var body = {
         url: ''
     };
-    //get myCanvas id
-    var myCanvas = $('#myCanvas');
+
     //create the image tag
     var addImg = $("<img>");
     //add the image tag into the emotion-img div
     $('#emotion-img').append(addImg);
-
-    var canvases = [];
+    //create the canvas element to be added in
+    var createCanvas = $('<canvas id="myCanvas" width="1" height="1"></canvas>')
 
     ////////////////////////////////////////////////////////
 
     //to iterate over the boxes data
     function displayBoxes(data) {
+
+        var myCanvas = $('#myCanvas');
         // var canvases = [];
-        // for (var canavs in canvases) {
-        //   myCanvas.clearCanvas();
-        // }
         //for each object in the array, each person
         for (var i = 0; i < data.length; i++) {
-            canvases.push([i]);
+            // canvases.push([i]);
             // console.log('object in data: ', data[i]); // this gets the individual object (one person's data)
             //Get faceRectangle
             var faceRectangleDim = data[i].faceRectangle;
             // console.log(faceRectangleDim);
-            var faceHappiness = data[i].scores.happiness;
+            // var faceHappiness = data[i].scores.happiness;
             // console.log(faceHappiness);
             // console.log('creating rectangle ', data[i].faceRectangle);
             // console.log('object[i].faceRectangle: ', faceRectangleDim); //get the dimentions of the faceRectangle
@@ -49,35 +50,87 @@ $(document).ready(function() {
                 cornerRadius: 1,
                 intangible: false,
                 click: function(layer) {
-                    $('#happiness')
-                    console.log('layerName: ',layer.name, " and happiness ", Math.round(data[layer.name].scores.happiness*100) )
-                    // console.log('layerName: ',layer.name, " and scores: ", JSON.stringify(data[layer.name].scores) )
+                  createBars = true;
+                    // console.log('layerName: ', layer.name, " and happiness ", Math.round(data[layer.name].scores.happiness * 100))
+                    // console.log('layerName: ', layer.name, " and scores: ", JSON.stringify(data[layer.name].scores))
+                    $('h5').css('visibility', 'visible');
+                    // $('.progressBar').css('display', 'block');
 
-                    // (function() {
-                        $('#happiness').progressbar({
-                            value: Math.round(data[layer.name].scores.happiness*100)
-                        });
-                    // })();
-
-                    // var individualScores = JSON.stringify(data[layer.name].scores.happiness);
-                    // happyDiv.text(individualScores);
+                    //HAPPINESS BAR
+                    $('#happiness').progressbar({
+                        value: Math.round(data[layer.name].scores.happiness * 100),
+                    });
+                    // $('#happiness .progressbarValue').css({"background": '#FF0000'});
+                    //SADNESS BAR
+                    $('#sadness').progressbar({
+                        value: Math.round(data[layer.name].scores.sadness * 100)
+                    });
+                    //SURPRISE BAR
+                    $('#surprise').progressbar({
+                        value: Math.round(data[layer.name].scores.surprise * 100)
+                    });
+                    //NEUTRAL BAR
+                    $('#neutral').progressbar({
+                        value: Math.round(data[layer.name].scores.neutral * 100)
+                    });
+                    //FEAR BAR
+                    $('#fear').progressbar({
+                        value: Math.round(data[layer.name].scores.fear * 100)
+                    });
+                    //DISGUST BAR
+                    $('#disgust').progressbar({
+                        value: Math.round(data[layer.name].scores.disgust * 100)
+                    });
+                    //ANGER BAR
+                    $('#anger').progressbar({
+                        value: Math.round(data[layer.name].scores.anger * 100)
+                    });
+                    //CONTEMPT BAR
+                    $('#contempt').progressbar({
+                        value: Math.round(data[layer.name].scores.contempt * 100)
+                    });
                 }
             });
         };
     };
 
-    function clearCanvases(canvases) {
-        for (var i = 0; i < canvases.length; i++) {
-            myCanvas.removeLayer('layer' + i).drawLayers();
-        }
-        // for (var canvas in canvases) {
-        //   myCanvas.removeLayer();
-        // }
-    }
-
+    // function clearCanvases(canvases) {
+    //     for (var i = 0; i < canvases.length; i++) {
+    //         myCanvas.removeLayer('layer' + i).drawLayers();
+    //     }
+    //     // for (var canvas in canvases) {
+    //     //   myCanvas.removeLayer();
+    //     // }
+    // }
 
     function urlSubmitClick() {
-        clearCanvases(canvases);
+      if (createBars === true) {
+        //HAPPINESS BAR
+        $('#happiness').progressbar('destroy');
+        //SADNESS BAR
+        $('#sadness').progressbar('destroy');
+        //SURPRISE BAR
+        $('#surprise').progressbar('destroy');
+        //NEUTRAL BAR
+        $('#neutral').progressbar('destroy');
+        //FEAR BAR
+        $('#fear').progressbar('destroy');
+        //DISGUST BAR
+        $('#disgust').progressbar('destroy');
+        //ANGER BAR
+        $('#anger').progressbar('destroy');
+        //CONTEMPT BAR
+        $('#contempt').progressbar('destroy');
+        createBars = false;
+      }
+
+        if ($('#myCanvas')) {
+            $('#myCanvas').remove()
+        };
+
+        createCanvas.prependTo('#emotion-img');
+        $('h5').css('visibility', 'hidden');
+        // $('.progressBar').css('display', 'none');
         // console.log(body, 'first body'); //shows the what was in the url value when we start
         //grab the value from the input area
         var inputVal = $('input').val();
@@ -87,20 +140,16 @@ $(document).ready(function() {
         // console.log(body); //this shows that the url has changed
         //change the body object into something JSON can read
         body = JSON.stringify(body);
-        //clear canvas so that the old boxes go away
-        // myCanvas.clearCanvas();
-
+        var myCanvas = $('#myCanvas');
         var image = new Image();
         image.src = inputVal;
         image.onload = function() {
-            // console.log(this.height, this.width)
-            //console.log($('canvas'));
+            console.log(this.height, this.width)
+            console.log($('canvas'));
             myCanvas.attr('height', this.height);
             myCanvas.attr('width', this.width);
         };
 
-
-        // console.log($('#emotion-img')); //make sure the img tag got into the div
         //clear the input value so it can accept a new one
         $('input').val('');
 
@@ -114,14 +163,15 @@ $(document).ready(function() {
             }
             //add the src to the img tag, the src being the input value
             $(addImg).attr("src", inputVal);
-            console.log(data); //log the data into the console
+            // console.log(data); //log the data into the console
             //parse the body back into an object for the next input
             body = JSON.parse(body);
+
 
             displayBoxes(data);
         });
         $xhr.fail(function(data) {
-            if ($xhr.status === 400) {
+            if ($xhr.status == 400) {
                 Materialize.toast('Please enter a valid URL', 4000);
                 return
             }
